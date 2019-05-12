eebe6849ed331a8dd001d7822de7e8057dea1be6
Tim Acker <tjacker31@gmail.com>
2019-05-11 23:49:02 -0400

    Refactors authService to work as express middleware

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,24 +1,24 @@
 const axios = require('axios');
-const { models } = require('../models');
 
-module.exports.fetchAuthToken = async (email, password) => {
+const authService = async (req, res, next) => {
   try {
-    console.log(email, password);
-    console.log(process.env.BCS_URL);
-    const { data } = await axios.post(`${process.env.BCS_URL}/login`, { email, password });
-    console.log(data);
-    const { authToken } = data.authenticationInfo;
-    return [null, authToken];
+    // Fetch data response from Boot Camp Spot (BCS) Login
+    const { data } = await axios.post(`${process.env.BCS_URL}/login`, req.body);
+
+    // If success is false throw error
+    if (!data.success) throw new Error();
+
+    // If success is true, add BCS userId and authToken to request object
+    const { userId, authToken } = data.authenticationInfo;
+    req.userId = userId;
+    req.authToken = authToken;
+
+    // Call next process in login route
+    next();
   } catch (err) {
-    console.log(err, 'err inside fetchAuthToken');
-    return [err, null];
+    // console.log(err);
+    res.status(401).send({ error: 'Invalid Credentials' });
   }
 };
 
-module.exports.insertAuthToken = async token => {
-  try {
-    await models.Session.create({ token });
-  } catch (err) {
-    return err;
-  }
-};
+module.exports = authService;
