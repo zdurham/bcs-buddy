6e287408191c87d5446e56da632ce17207a52e56
Tim Acker <tjacker31@gmail.com>
2019-05-17 19:31:30 -0400

    Updates mongoose connection with 'useFindAndModify: false' to remove deprecation warning

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 module.exports.connect = () => {
   return mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
   });
 };
 
