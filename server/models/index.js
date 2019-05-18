015dbe8cbfdbc41d0fee378825c6a8decb450b52
Tim Acker <tjacker31@gmail.com>
2019-05-18 12:01:08 -0400

    Changes MONGODB_URL process variable to MONGODB_URI

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports.connect = () => {
-  return mongoose.connect(process.env.MONGODB_URL, {
+  return mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
